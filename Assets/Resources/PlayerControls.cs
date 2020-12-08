fa119cb54c2efd00500843973d45772d4491fbb8
Carson <mr.carson.fisher@gmail.com>
2020-12-08 15:33:52 -0700

    Added movement to character

---
+++ Assets/Resources/PlayerControls.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public GameObject EventObject;
+    private InputHandler ip;
+    float xvel = 0f;
+    float yvel = 0f;
+
+
+
+    float speed = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventObject = GameObject.Find("EventObject");
+        ip = EventObject.GetComponent<InputHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterMovement();
+    }
+
+    void characterMovement()
+    {
+        HashSet<string> kp = ip.getKeysPressed();
+        foreach (string key in kp)
+        {
+            if (key.Equals("W") || key.Equals("UpArrow"))
+            {
+                yvel += speed;
+            }
+            if (key.Equals("S") || key.Equals("DownArrow"))
+            {
+                yvel += -speed;
+            }
+            if (key.Equals("D") || key.Equals("RightArrow"))
+            {
+                xvel += speed;
+            }
+            if (key.Equals("A") || key.Equals("LeftArrow"))
+            {
+                xvel += -speed;
+            }
+        }
+        this.transform.position = this.transform.position + new Vector3(xvel, yvel);
+        xvel = 0;
+        yvel = 0;
+        print(this.transform.position);
+    }
+}
