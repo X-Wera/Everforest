035ef052096fe516aeda45eb039393429f00ec10
Carson <mr.carson.fisher@gmail.com>
2020-12-14 17:26:08 -0700

    code cleanup

--- Assets/Resources/Game/World/InitializeWorld.cs
+++ Assets/Resources/Game/World/InitializeWorld.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InitializeWorld: MonoBehaviour
+public class InitializeWorld : MonoBehaviour
 {
-    public GameObject grid;
+    public Grid grid;
+    //public Tilemap tilemap;
     // Start is called before the first frame update
     void Start()
     {
+        initGrid();
         createHub();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void initGrid()
+    {
+        grid = new GameObject("Grid").AddComponent<Grid>();
+        //var tilemap = new GameObject("Tilemap").AddComponent<Tilemap>();
+        //tilemap.transform.SetParent(grid.gameObject);
     }
 
     void createHub()
